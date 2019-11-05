0e0bccc6300de198579ca7966be0d747d7a93b0f
Thomas Løye Skafte <s153047@student.dtu.dk>
2019-11-05 15:35:32 +0100

    fix cooldown

---
+++ Assets/Scripts/EnemyStats.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int health = 100;
+
+
+    public void TakeDamage (int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Dead();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Dead()
+    {
+    	Destroy(gameObject);
+    }
+}
