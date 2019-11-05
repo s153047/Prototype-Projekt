5bb1fd4923e286f504c2bf85bb36f06c9692994b
Thomas Løye Skafte <s153047@student.dtu.dk>
2019-11-05 11:36:32 +0100

    Added tilemap collision

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+	Rigidbody2D rigidbody2d;
     // Start is called before the first frame update
     void Start()
     {
+    	rigidbody2d = GetComponent<Rigidbody2D>();
     }
     // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        Vector2 position = transform.position;
+
+        Vector2 position = rigidbody2d.position;
         position.x += 3.0f * horizontal * Time.deltaTime;
         position.y += 3.0f * vertical * Time.deltaTime;
-        transform.position = position;
+
+        rigidbody2d.position = position;
     }
 }
