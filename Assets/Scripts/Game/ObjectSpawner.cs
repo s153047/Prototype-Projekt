c6d77ae2e021fe94894c6ebea53d5130664bf1d5
Thomas Løye Skafte <s153047@student.dtu.dk>
2019-12-02 17:25:30 +0100

    Object spawner

---
+++ Assets/Scripts/Game/ObjectSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+
+	public GameObject[] objects;
+
+	// the range of X
+	[Header ("X Spawn Range")]
+	public float xMin;
+	public float xMax;
+
+	// the range of y
+	[Header ("Y Spawn Range")]
+	public float yMin;
+	public float yMax;
+
+	void Start(){
+		int objAmount = Random.Range(0,4);
+		for (int i = 0; i < objAmount; i++){
+			spawnObject();
+		}
+	}
+
+	void Update(){}
+
+	void spawnObject(){
+		Vector3 pos = new Vector3(Random.Range(xMin, xMax), Random.Range(yMin, yMax), 0);
+
+		GameObject obj = objects[Random.Range(0,objects.Length)];
+
+		Instantiate(obj, pos, Quaternion.identity);
+	}
+
+}
