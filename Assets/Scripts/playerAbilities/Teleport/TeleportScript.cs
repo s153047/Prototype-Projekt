bc026c36b2eb2e98d9da9106663627c767eb3011
Thomas Løye Skafte <s153047@student.dtu.dk>
2019-12-14 15:43:44 +0100

    Fixed teleport ranges

--- Assets/Scripts/playerAbilities/Teleport/TeleportScript.cs
+++ Assets/Scripts/playerAbilities/Teleport/TeleportScript.cs
@@ -8,9 +8,10 @@ public class TeleportScript : Abilities
 	//public float range = 2f;
 	public override bool CastSpell(GameObject player, Transform firePoint){
 		if (!base.CastSpell(player, firePoint)) return false;
-		Vector3 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-		target.z = 0;
-		target.y -= 0.5f;
+		Vector3 targetTmp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Vector3 target = new Vector3(Mathf.Clamp(targetTmp.x,-7.6f,7.6f),Mathf.Clamp(targetTmp.y, -4f, 3.6f),0f);
+		//target.y -= 0.5f;
+
 		player.transform.position = target;
 		return true;
     }
