3616c82f5dcab249a48a3812c55c31f6ee24da67
Jacob-Chen-Jensen <jacobchenjensen@gmail.com>
2019-12-14 20:57:09 +0100

    Main menu ...

---
+++ Assets/Scripts/Button/MainMenuToGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class MainMenuToGame : MonoBehaviour
+{
+    public void TaskOnClick()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
